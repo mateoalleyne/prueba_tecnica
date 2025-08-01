71f9ef2dbf8d16c688712c79bc689bb8770ccc08
Mateo Alleyne <67911058+mateoalleyne@users.noreply.github.com>
2025-08-01 17:05:56 -0500

    Delete punto_2_ directory

--- punto_2_/db_init.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS vector;
-
-CREATE TABLE IF NOT EXISTS movies (
-    id SERIAL PRIMARY KEY,
-    title TEXT,
-    plot TEXT,
-    image TEXT,
-    embedding VECTOR(1536)
-);

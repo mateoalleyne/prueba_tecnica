db88d77d1b2cebecf13c1ac1ecd0f0f6be73c1e5
Juul <mateoalleyne@gmail.com>
2025-08-02 13:44:58 -0500

    punto 2 con Postgres desplegado en RDS de AWS

--- punto_2/load_vectors.py
+++ punto_2/load_vectors.py
@@ -1,29 +1,56 @@
 import psycopg2
-import openai
 from embedding import get_embedding
 import pandas as pd
 
 connection=psycopg2.connect(
-    host='localhost',
+    # host='localhost',
+    host='movies.cqf44we820b0.us-east-1.rds.amazonaws.com',
+    port=5432,
     user='postgres',
-    password='luisma129',
-    database='movies'
+    password='c0ntr453n4',
+    database='postgres'
 )
 cur=connection.cursor()
-
 data=pd.read_csv('movies-dataset.csv')
-for i,row in data.iterrows():
-    text=f"{row['title']} - {row['image']} : {row['plot']}"
-    print(row['title'])
-    emb=get_embedding(text)
+
+
+def init_db(data=data):
     cur.execute(
-        """INSERT INTO movies (title, plot, image, embedding) VALUES (%s, %s, %s, %s)""",
-        (row['title'], row['plot'], row['image'], emb)
-    )
+    """
+    CREATE EXTENSION IF NOT EXISTS vector;
+
+    CREATE TABLE IF NOT EXISTS movies (
+        id SERIAL PRIMARY KEY,
+        title TEXT,
+        plot TEXT,
+        image TEXT,
+        embedding VECTOR(1536)
+    );
+    """)
+    for i,row in data.iterrows():
+        text=f"{row['title']} - {row['image']} : {row['plot']}"
+        print(row['title'])
+        emb=get_embedding(text)
+        cur.execute(
+            """INSERT INTO movies (title, plot, image, embedding) VALUES (%s, %s, %s, %s)""",
+            (row['title'], row['plot'], row['image'], emb)
+        )
+
+
+cur.execute("SELECT COUNT(1) FROM movies")
+size=cur.fetchall()
+print(size)
+
+try:
+    init_db() if size[0,0]==0 else None
+except:
+    pass
 
 # text=f"{data.iloc[0,0]} - {data.iloc[0,1]} : {data.iloc[0,2]}"
-# # emb=get_embedding(text)
+# emb=get_embedding(text)
 # print(f'{text}\n{emb}')
+
+# )
 # cur.execute(
 #         """INSERT INTO movies (title, plot, image, embedding) VALUES (%s, %s, %s, %s)""",
 #         (data.iloc[0,0], data.iloc[0,1], data.iloc[0,2], emb)
--- punto_2/vector_store.py
+++ punto_2/vector_store.py
@@ -6,10 +6,12 @@ def query_similar_movies(question,top_k=3):
     embedding = get_embedding(question)
 
     connection=psycopg2.connect(
-    host='localhost',
-    user='postgres',
-    password='luisma129',
-    database='movies'
+        # host='localhost',
+        host='movies.cqf44we820b0.us-east-1.rds.amazonaws.com',
+        port=5432,
+        user='postgres',
+        password='c0ntr453n4',
+        database='postgres'
     )
     cur=connection.cursor()
 
